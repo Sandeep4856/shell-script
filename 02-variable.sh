4c30099fa9c7aae108a1d857e8f6d2e256a6080d
Sandeep4856 <sandeepkumardarla001@gmail.com>
2024-08-05 14:02:01 +0530

    hello script

---
+++ 02-variable.sh
@@ -0,0 +1,6 @@
+!# /bin/bash
+
+echo "ramesh:: hello suresh, how are you?"
+echo "suresh:: hi ramesh, iam fine. how are you?"
+echo "Ramesh:: Iam fine too. How is your work?"
+echo "suresh:: Iam learing devops"
