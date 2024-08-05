68cc191259a72a1c369de861496a5a3dbd622abd
Sandeep4856 <sandeepkumardarla001@gmail.com>
2024-08-05 14:10:15 +0530

    hello script

--- 02-variable.sh
+++ 02-variable.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
 
-echo "ramesh:: hello suresh, how are you?"
-echo "suresh:: hi ramesh, iam fine. how are you?"
-echo "Ramesh:: Iam fine too. How is your work?"
-echo "suresh:: Iam learing devops"
+PERSON1=ramesh
+PERSON2=suresh
+
+echo "$PERSON1:: hello $PERSON2, how are you?"
+echo "$PERSON2:: hi $PERSON1, iam fine. how are you?"
+echo "$PERSON1:: Iam fine too. How is your work?"
+echo "$PERSON2:: Iam learing devops"
