bb773f5daf4df81cbad5ea8f695797bca2108e13
Sandeep4856 <sandeepkumardarla001@gmail.com>
2024-08-05 21:19:49 +0530

    some-message

--- 02-variable.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-PERSON1=SAMHITH
-PERSON2=SOHAN
-
-echo "$PERSON1:: hello $PERSON2, how are you?"
-echo "$PERSON2:: hi $PERSON1, iam fine. how are you?"
-echo "$PERSON1:: Iam fine too. How is your work?"
-echo "$PERSON2:: Iam learing devops"
