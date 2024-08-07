cff9451fe8974098e0956bf3435444313ba6f9d2
Sandeep4856 <sandeepkumardarla001@gmail.com>
2024-08-07 16:37:06 +0530

    hello script

---
+++ 04-variable.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#variable declaration
+PERSON1=$1
+PERSON2=$2
+
+#referring a variable
+echo "$PERSON1:: hello $PERSON2, how are you?"
+echo "$PERSON2:: hi $PERSON1, iam fine. how are you?"
+echo "$PERSON1:: Iam fine too. How is your work?"
+echo "$PERSON2:: Iam learing devops"
+
