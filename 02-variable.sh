2ccda6e259995de1b129daa9e6afa320bc77b60b
Sandeep4856 <sandeepkumardarla001@gmail.com>
2024-08-07 16:33:43 +0530

    hello script

--- 02-variable.sh
+++ 02-variable.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+#variable declaration
 PERSON1=rajesh
 PERSON2=Dinesh
 
+#referring a variable
 echo "$PERSON1:: hello $PERSON2, how are you?"
 echo "$PERSON2:: hi $PERSON1, iam fine. how are you?"
 echo "$PERSON1:: Iam fine too. How is your work?"
